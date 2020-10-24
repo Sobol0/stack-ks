722dd71b0c257a0e2cb4ca74c0a485e6bb5ef186
Uzytkownik <uzytkownik@kursGIT.pl>
2020-10-24 12:03:19 +0200

    Itial commit

---
+++ Tests/test_stack.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+
+from stack import Stack
+
+class TestStackOperations(TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def tearDown(self):
+        self.stack.clear()
+
+    def test_should_add_new_element_to_stack(self):
+        value, expected = 42, 42
+
+        self.stack.push(value)
+
+        self.assertEqual(self.stack.peek(), expected)
+
+    def test_should_remove_last_element_from_stack(self):
+        element, expected = 42, 42
+        self.stack.push(element)
+
+        value = self.stack.pop()
+
+        self.assertEqual(value, expected)
+        self.assertEqual(self.stack.size, 0)
+
+    def test_peek_should_show_last_element_and_not_remove_it(self):
+        element, expected = 42, 42
+        self.stack.push(element)
+
+        value = self.stack.peek()
+
+        self.assertEqual(value, expected)
+        self.assertEqual(self.stack.size, 1)
+
+    def test_should_clear_stack(self):
+        self.stack.push(42)
+        self.stack.push(43)
+
+        self.stack.clear()
+
+        self.assertEqual(self.stack.size, 0)
+
+    def test_should_raise_when_calles_pop_on_empty_stack(self):
+        self.fail("Not implemented yet")
+
+
